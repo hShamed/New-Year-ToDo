ea585733207aec19bf9206f3a828046b00eeffcc
hShamed <h.shamed.ar@gmail.com>
2024-01-09 15:13:29 -0600

    Creación del archivo index y sus estilos

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New Year</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header class="top">
+        <p> 
+            Mis Propósitos 🎉
+        </p>
+    </header>
+
+    <div class="content">
+        <main class="leftContent">
+            <form action="">            
+                <label for="">Propósito</label>
+                <input type="text">
+
+                <label for="">Descripción</label>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+
+                <button type="submit">Guardar</button>
+            </form>
+        </main>
+        <aside class="rightContent">
+            <table class="objectivesTable">
+                <tr class="objectivesHeader">
+                    <th>Propósito</th>
+                    <th>Descripción</th>
+                    <th>Fecha de Creación</th>
+                    <th>Acciones</th>
+                </tr>
+                <tr>
+                    <td>Ejercicio</td>
+                    <td>Rutina One Punch</td>
+                    <td>09/01/2024 03:08:50</td>
+                    <td>
+                        <button class="btnEdit"></button>
+                        <button class="btnDelete"></button>
+                    </td>
+                </tr>
+            </table>
+        </aside>
+    </div>
+
+    
+</body>
+</html>
